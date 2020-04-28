3ffd09f6969c33ce5ffc6a60d9a14523ac6964bd
StuartCowley <voidstalker@live.co.uk>
2020-04-28 23:23:46 +0100

    updated packages

---
+++ public/component---src-pages-404-js-67bffec91173c05d13d7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"8ypT":function(e,t,n){},w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=(n("8ypT"),function(e){var t=e.children;return l.a.createElement("div",null,t)}),r=n("vrFN");t.default=function(){return l.a.createElement(u,null,l.a.createElement(r.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-67bffec91173c05d13d7.js.map
